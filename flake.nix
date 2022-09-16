425ab23694b3420d575c43f78766a17f4749dc0d
Yusef Napora <yusef@napora.org>
2022-09-16 12:20:13 -0400

    add libp2p-core, add native deps to flake.nix

--- flake.nix
+++ flake.nix
@@ -10,16 +10,23 @@
       let
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
+
+        # native deps needed to build libp2p-core
+        native-deps = with pkgs; [ openssl.dev protobuf ];
       in
       {
-        defaultPackage = naersk-lib.buildPackage ./.;
+        defaultPackage = naersk-lib.buildPackage {
+          src = ./.;
+
+          nativeBuildInputs = native-deps;
+        };
 
         defaultApp = utils.lib.mkApp {
           drv = self.defaultPackage."${system}";
         };
 
         devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ] ++ native-deps;
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
       });
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod name;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
