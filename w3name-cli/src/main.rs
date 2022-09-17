5066d2d5b520ffbc8457968e13df5b933a9d056a
Yusef Napora <yusef@napora.org>
2022-09-17 18:00:02 -0400

    scaffold out cli app

---
+++ w3name-cli/src/main.rs
@@ -0,0 +1,37 @@
+use std::path::PathBuf;
+use clap::{Parser, Subcommand};
+
+mod resolve;
+
+use crate::resolve::resolve;
+
+#[derive(Parser)]
+#[clap(version, about, long_about = None)]
+struct Cli {
+  #[clap(subcommand)]
+  command: Commands
+}
+
+#[derive(Subcommand)]
+enum Commands {
+  Resolve {
+
+    #[clap(value_parser)]
+    name: String,
+  },
+
+  // TODO: publish
+}
+
+#[tokio::main]
+async fn main() {
+  let cli = Cli::parse();
+  match &cli.command {
+    Commands::Resolve { name } => {
+      let value = resolve(name).await.expect("resolve error");
+      println!("{}", value);
+    },
+
+    // TODO: publish
+  }
+}
---
+++ w3name-cli/src/resolve.rs
@@ -0,0 +1,6 @@
+use std::error::Error;
+
+pub async fn resolve(name_str: &str) -> Result<String, Box<dyn Error>> {
+
+  todo!()
+}
