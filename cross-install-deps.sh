bcd23603b2838b2efff4ba89c24d610efe235cda
Yusef Napora <yusef@napora.org>
2022-09-18 21:56:50 -0400

    download correct protoc for aarch64

--- cross-install-deps.sh
+++ cross-install-deps.sh
@@ -4,8 +4,9 @@ apt-get update
 apt-get install -y curl unzip
 
 PROTOC_VERSION="3.20.2"
+ARCH=$(uname -m)
 PB_REL="https://github.com/protocolbuffers/protobuf/releases"
-ZIPFILE="protoc-$PROTOC_VERSION-linux-x86_64.zip"
+ZIPFILE="protoc-$PROTOC_VERSION-linux-$ARCH.zip"
 curl -LO $PB_REL/download/v$PROTOC_VERSION/$ZIPFILE
 
 mkdir -p /opt/protobuf
