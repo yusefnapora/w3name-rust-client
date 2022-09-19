fc88c01c67e47c3de69354823e629b264e271920
Yusef Napora <yusef@napora.org>
2022-09-19 15:32:24 -0400

    add todo comment about publish error handling

--- w3name-cli/src/main.rs
+++ w3name-cli/src/main.rs
@@ -7,6 +7,7 @@ use w3name::{Name, Revision, W3NameClient, WritableName};
 
 #[derive(Parser)]
 #[clap(name = "w3name", version, about, long_about = None)]
+/// A tool for creating verifiable names in a web3 world.
 struct Cli {
   #[clap(subcommand)]
   command: Commands,
@@ -14,20 +15,31 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
+  /// Lookup the current value for a name record.
   Resolve {
+    /// The name identifier, e.g. "k51qzi5uqu5dka3tmn6ipgsrq1u2bkuowdwlqcw0vibledypt1y9y5i8v8xwvu"
     #[clap(value_parser)]
     name: String,
   },
 
+  /// Publish a new value for a name, signed with the name's private key.
   Publish {
+    /// Path to a key file (see the `create` command to make one).
     #[clap(short, long, value_parser, value_name = "KEY_FILE")]
     key: PathBuf,
 
+    /// The value to publish.
     #[clap(short, long, value_parser)]
     value: String,
   },
 
+  /// Create a new public/private keypair and save it to disk.
   Create {
+
+    /// Filename to write the key to.
+    /// 
+    /// If not given, will write to a file named `<name>.key`,
+    /// where `<name>` is the string form of the public key.
     #[clap(short, long, value_parser)]
     output: Option<PathBuf>,
   },
@@ -88,6 +100,8 @@ async fn publish(key_file: &PathBuf, value: &str) -> Result<(), CliError> {
   // to avoid having to keep old revisions around, we first try to resolve and increment any existing records
   let new_revision = match client.resolve(&writable.to_name()).await {
     Ok(revision) => revision.increment(value),
+
+    // TODO: only fallback to v0 if the name doesn't exist; bail on other errors
     Err(_) => Revision::v0(&writable.to_name(), value),
   };
 
